c32158d3c1357e68fe7200dcd2f80c3eb7b84aa7
taihungg <hunginception@gmail.com>
2025-11-15 21:26:22 +0700

    create static final variants and constructor

--- src/main/java/com/amby/core/AmbYContract.java
+++ src/main/java/com/amby/core/AmbYContract.java
@@ -1,33 +1,41 @@
 package com.amby.core;
 
 import io.neow3j.devpack.Hash160;
+import io.neow3j.devpack.Runtime;
 import io.neow3j.devpack.Storage;
-import static io.neow3j.devpack.Storage.getReadOnlyContext;
-import static io.neow3j.devpack.Storage.getStorageContext;
-import static io.neow3j.devpack.StringLiteralHelper.addressToScriptHash;
+import io.neow3j.devpack.StorageContext;
+import io.neow3j.devpack.Transaction;
 import io.neow3j.devpack.annotations.DisplayName;
 import io.neow3j.devpack.annotations.ManifestExtra;
 import io.neow3j.devpack.annotations.OnDeployment;
+import io.neow3j.devpack.annotations.Permission;
+import io.neow3j.devpack.events.Event1Arg;
 
-@DisplayName("HelloWorld")
-@ManifestExtra(key = "author", value = "Your Name")
+@DisplayName("AmbYThePredictMarket")
+@ManifestExtra(key = "author", value = "BrianNg")
+@Permission(contract = "*")
 public class AmbYContract {
-
-    static final byte[] ownerKey = new byte[]{0x00};
-
-    static final String staticValue = "Hello World!";
+    static final String KEY_OWNER = "owner";
+    static final String KEY_MARKET_COUNT = "count";
+    static final String KEY_PAUSED = "paused";
 
     @OnDeployment
     public static void deploy(Object data, boolean update) {
-        Storage.put(getStorageContext(), ownerKey, addressToScriptHash("NNSyinBZAr8HMhjj95MfkKD1PY7YWoDweR"));
+        if (update) return;
+
+        StorageContext ctx = Storage.getStorageContext();
+
+        Transaction tx = (Transaction) Runtime.getScriptContainer();
+
+        Hash160 deployer = tx.sender;
+
+        Storage.put(ctx, KEY_OWNER, deployer);
+        onOwnerSet.fire(deployer);
+
+        Storage.put(ctx, KEY_MARKET_COUNT, 0);
+
+        Storage.put(ctx, KEY_PAUSED, 0);
     }
 
-    public static Hash160 getOwner() {
-        return Storage.getHash160(getReadOnlyContext(), ownerKey);
-    }
-
-    public static String getStaticValue() {
-        return staticValue;
-    }
-
+    static Event1Arg<Hash160> onOwnerSet;
 }
